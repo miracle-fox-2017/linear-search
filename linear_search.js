84b5d672eba8cc84024ff2d47fa82e978ada62d2
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-17 15:49:07 +0700

    Linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,16 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let index = [];
+  let length = values.length;
+
+  for (var i = 0; i < values.length; i++) {
+  	if (values[i] == target) {
+  		index.push(i);
+  	} 
+  }
+
+  return (index.length > 0) ? index[0] : -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +26,16 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+	let index = [];
+	let length = values.length;
+
+	for (var i = 0; i < values.length; i++) {
+		if (values[i] == target) {
+			index.push(i);
+		} 
+	}
+	
+	return index;
 }
 
 let banana_arr = "banana".split("");

7ee6daa5ab130617c78b9e43e4e8049b0ab8a323
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-10-17 18:39:05 +0700

    Tugas Linear Search

--- linear_search.js
+++ linear_search.js
@@ -1,30 +1,42 @@
-'use strict'
-
-// Release 0
+ 'use strict'
 console.log("Linear Search")
+
 let linearSearch = (target, values) => {
-  //write your code here
+  for(var i = 0 ; i < random_numbers.length ; i++) {
+    if(random_numbers[i] === target) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
-// 2
-console.log(linearSearch(9, random_numbers));
-// -1
+ console.log(linearSearch(18, random_numbers)); // 2
+ console.log(linearSearch(72, random_numbers)); // 4
+ console.log(linearSearch(9, random_numbers)); // -1
 
 // Release 1
 console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
-  //write your code here
-}
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
+
+let globalLinearSearch = (target, values) => {
+  let hasil = '';
+  let arr = [];
+    for(var i = 0; i < banana_arr.length ; i++) {
+      if(target === banana_arr[i]) {
+        arr.push(i);
+      }
+    }
+    return arr;
+}
+
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }

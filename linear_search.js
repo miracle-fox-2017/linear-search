bafe1fbe5267293541548cc85915d4e160f02d3b
Yofri <yofriadiyahya@gmail.com>
2017-10-17 16:53:54 +0700

    Challenge done

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+
+  for (i in values) {
+    if (target === values[i]) return i;
+  }
+
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  const arrResult = [];
+
+  for (i in values) {
+    if (target === values[i]) arrResult.push(Number(i));
+  }
+
+  return arrResult;
 }
 
 let banana_arr = "banana".split("");

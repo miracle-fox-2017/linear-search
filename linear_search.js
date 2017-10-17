77099168c701e465365b21993913f2c0964ce735
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-17 15:41:12 +0700

    release 1

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+  	if (target === values[i]) {
+  		return i
+  	}
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,17 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let arrIndex = []
+  for (let i = 0; i < values.length; i++) {
+  	if (target === values[i]) {
+  		arrIndex.push(i)
+  	}
+  }
+
+  if (arrIndex.length != 0) {
+  	return arrIndex
+  }
+  return -1
 }
 
 let banana_arr = "banana".split("");

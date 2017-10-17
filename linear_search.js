63c3c2309baa86743f4fc011eab5b4c731802e00
Ayunda <ayundahamzah@gmail.com>
2017-10-17 18:17:22 +0700

    release 1

--- linear_search.js
+++ linear_search.js
@@ -17,19 +17,25 @@ console.log(linearSearch(18, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
 // -1
-//
-// // Release 1
-// console.log("Global Linear Search")
-// let globalLinearSearch = (target, values) => {
-//   //write your code here
-// }
-//
-// let banana_arr = "banana".split("");
-// // ["b", "a", "n", "a", "n", "a"]
-// console.log(globalLinearSearch("a", banana_arr));
-// // [ 1, 3, 5 ]
-//
-// module.exports = {
-//   linearSearch,
-//   globalLinearSearch
-// }
+
+// Release 1
+console.log("Global Linear Search")
+let globalLinearSearch = (target, values) => {
+  var hasil=[];
+  for(var i=0;i<values.length;i++){
+    if(target===values[i]){
+      hasil.push(i);
+    }
+   }
+   return hasil;
+}
+
+let banana_arr = "banana".split("");
+// ["b", "a", "n", "a", "n", "a"]
+console.log(globalLinearSearch("a", banana_arr));
+// [ 1, 3, 5 ]
+
+module.exports = {
+  linearSearch,
+  globalLinearSearch
+}

c2491716d0d23e0ca1288152cca65be7f68d55cc
Faris Widyantho <radenfaris@gmail.com>
2017-10-17 14:42:24 +0700

    release 0 & 1

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,16 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var pointer = 0;
+  while(values.length > pointer){
+    //cek random_numbers == target, pointer juga disimpan
+    if(values[pointer] == target){
+      return pointer;
+    } else {
+      pointer += 1;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +27,16 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var result = [];
+  var pointerGlobal = 0;
+  while(values.length > pointerGlobal){
+    if(values[pointerGlobal] == target){
+      result.push(pointerGlobal);
+    }
+    pointerGlobal = pointerGlobal + 1;
+
+  }
+  return result;
 }
 
 let banana_arr = "banana".split("");
